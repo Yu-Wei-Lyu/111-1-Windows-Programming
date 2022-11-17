d8a29b39dae5670ff50b95d21f0e05017aac4096
YWL_Desktop <awaia732@gmail.com>
2022-11-17 23:26:12 +0800

    HW5

--- HW5/Library109590004GUITests/UnitTest1.cs
+++ HW5/Library109590004GUITests/UnitTest1.cs
@@ -2,27 +2,42 @@
 using System;
 using System.IO;
 
-namespace Library109590004GUITests
+namespace Library109590004
 {
     [TestClass()]
     public class UnitTest1
     {
+        private Robot _robot;
         private string targetAppPath;
-        private const string START_UP_FORM = "StartUpForm";
+        private const string START_UP_FORM = "MenuForm";
 
         // init
         [TestInitialize]
         public void Initialize()
         {
-            var projectName = "CourseSystem";
+            var projectName = "Library109590004";
             string solutionPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
-            targetAppPath = Path.Combine(solutionPath, projectName, "bin", "Debug", "CourseSystem.exe");
+            targetAppPath = Path.Combine(solutionPath, projectName, "bin", "Debug", "Library109590004.exe");
             _robot = new Robot(targetAppPath, START_UP_FORM);
         }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            _robot.CleanUp();
+        }
+
+        private void RunScriptAdd()
+        {
+            _robot.ClickButton("_borrowingSystemButton");
+        }
+
 
         [TestMethod]
         public void TestMethod1()
         {
+            RunScriptAdd();
+            _robot.AssertText("_borrowLabel", "借書單");
         }
     }
 }
