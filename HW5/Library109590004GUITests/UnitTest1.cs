5b254f1c5677240f90ee5784beb36b30a7ab0a9c
Yu-Wei,Lyu <awaia732@gmail.com>
2022-11-17 16:45:30 +0800

    HW5 tes

---
+++ HW5/Library109590004GUITests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace Library109590004GUITests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        private string targetAppPath;
+        private const string START_UP_FORM = "StartUpForm";
+
+        // init
+        [TestInitialize]
+        public void Initialize()
+        {
+            var projectName = "CourseSystem";
+            string solutionPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
+            targetAppPath = Path.Combine(solutionPath, projectName, "bin", "Debug", "CourseSystem.exe");
+            _robot = new Robot(targetAppPath, START_UP_FORM);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
