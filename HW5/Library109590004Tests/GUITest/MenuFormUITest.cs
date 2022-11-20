30d133ba1f4fc1c99af6e5e2c08e0699c60daed0
YWL_Desktop <awaia732@gmail.com>
2022-11-20 10:32:22 +0800

    save

--- HW5/Library109590004Tests/GUITest/MenuFormUITest.cs
+++ HW5/Library109590004Tests/GUITest/MenuFormUITest.cs
@@ -53,5 +53,33 @@ namespace Library109590004Tests
             _robot.AssertMessageBoxText("【暴力犯罪的大腦檔案 : 從神經犯罪學探究惡行的生物根源, 慎思以治療取代懲罰的未來防治計畫】2本\r\n\r\n已成功借出！");
             _robot.CloseMessageBox();
         }
+
+        // TestMethod
+        [TestMethod()]
+        public void TestMethod2()
+        {
+            _robot.ClickButtonByText("Book Borrowing System");
+            _robot.SwitchTo("BookBorrowingForm");
+            _robot.ClickButtonByText("查看我的背包");
+            _robot.SwitchTo("BookBorrowingForm");
+            _robot.ClickButtonByName("Book 1");
+            _robot.AssertText("_bookRemainLabel", "剩餘數量：1");
+            _robot.ClickButtonByText("加入借書單");
+            _robot.ClickButtonByText("確認借書");
+            _robot.CloseMessageBox();
+            _robot.ClickButtonByName("Book 1");
+            _robot.AssertText("_bookRemainLabel", "剩餘數量：0");
+            _robot.SwitchTo("BackPackForm");
+            string[] expectedStringArray = new string[] { "歸還", "1", "創造快樂大腦 : 重塑大腦快樂習慣-提升血清素, 多巴胺, 催產素, 腦內啡", "1", string.Format("{0:yyyy/MM/dd}", DateTime.Now), string.Format("{0:yyyy/MM/dd}", DateTime.Now.AddDays(30)), "176.51 8564 2022", "羅瑞塔.葛蕾吉亞諾.布魯", "閱樂國際文化出版" };
+            _robot.AssertDataGridViewRowDataBy("_backPackDataView", 0, expectedStringArray);
+            //_robot.ClickDataGridViewCellBy("_backPackDataView", 0, "還書");
+            //_robot.CloseMessageBox();
+            //_robot.AssertDataGridViewRowCountBy("_backPackDataView", 0);
+            ///_robot.CloseWindow();
+            //_robot.SwitchTo("BookBorrowingForm");
+            //_robot.ClickButtonByName("Book 1");
+            //_robot.AssertText("_bookRemainLabel", "剩餘數量：1");
+            //_robot.CloseMessageBox();
+        }
     }
 }
