f520e194e5d73a21a55b12dbcb0a9cc5e894a302
Yu-Wei,Lyu <awaia732@gmail.com>
2022-12-29 02:39:59 +0800

    .

--- HW7/DrawingForm/DrawingFormTests/PresentationModel/FormPresentationModelTests.cs
+++ HW7/DrawingForm/DrawingFormTests/PresentationModel/FormPresentationModelTests.cs
@@ -38,5 +38,73 @@ namespace DrawingForm.Presentation.Tests
             _presentationModel.Draw(_mockGraphics.Object);
             _mockGraphics.Verify(obj => obj.DrawTriangle(1, 1, 2, 2));
         }
+
+        [TestMethod()]
+        public void TestFormPresentationModel()
+        {
+            Assert.IsTrue(_presentationModel.IsLineButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsTriangleButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsRectangleButtonEnabled);
+        }
+
+        [TestMethod()]
+        public void TestNotifyPropertyChanged()
+        {
+            int count = 0;
+            _presentationModel.NotifyPropertyChanged("Nothing");
+            Assert.AreEqual(0, count);
+
+
+        }
+
+        [TestMethod()]
+        public void TestHandleRectangleButtonClick()
+        {
+            _presentationModel.HandleRectangleButtonClick();
+            Assert.IsTrue(_presentationModel.IsLineButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsTriangleButtonEnabled);
+            Assert.IsFalse(_presentationModel.IsRectangleButtonEnabled);
+        }
+
+        [TestMethod()]
+        public void TestHandleLineButtonClick()
+        {
+            _presentationModel.HandleLineButtonClick();
+            Assert.IsFalse(_presentationModel.IsLineButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsTriangleButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsRectangleButtonEnabled);
+        }
+
+        [TestMethod()]
+        public void TestHandleTriangleButtonClick()
+        {
+            _presentationModel.HandleTriangleButtonClick();
+            Assert.IsTrue(_presentationModel.IsLineButtonEnabled);
+            Assert.IsFalse(_presentationModel.IsTriangleButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsRectangleButtonEnabled);
+        }
+
+        [TestMethod()]
+        public void TestHandleClearButtonClick()
+        {
+            Assert.IsTrue(_presentationModel.IsLineButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsTriangleButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsRectangleButtonEnabled);
+        }
+
+        [TestMethod()]
+        public void TestSetToDefaultButtonEnabled()
+        {
+            _presentationModel.SetToDefaultButtonEnabled();
+            Assert.IsTrue(_presentationModel.IsLineButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsTriangleButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsRectangleButtonEnabled);
+            _model.SetStateLine();
+            _model.PressedPointer(5, 5);
+            _model.ReleasedPointer(8, 8);
+            Assert.IsFalse(_presentationModel.IsLineButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsTriangleButtonEnabled);
+            Assert.IsTrue(_presentationModel.IsRectangleButtonEnabled);
+        }
     }
 }
